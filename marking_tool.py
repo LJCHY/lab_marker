1a9c4210d6833a3c3571332b8898672e409a216a
LJCHY <109228480+LJCHY@users.noreply.github.com>
2025-10-18 00:18:44 +0800

    Update marking_tool.py

--- marking_tool.py
+++ marking_tool.py
@@ -49,7 +49,7 @@ presentation_options = {
     "bad_not_pdf": "The submitted file is NOT a PDF.",
     "bad_no_template": "The submitted file didn't follow(or use) the provided markdown file as its template (e.g. the cover sheet should be a separate page).",
     "bad_too_long": "The submitted file has more than 80 pages in total.",
-    "bad_filename": "The submitted file name does NOT follow the format of studentid_firstname_labs1_5.pdf",
+    "bad_filename": "The submitted file name does NOT follow the format of studentid_firstname_labs6_9.pdf",
     "bad_structure": "The submitted file has a poorly/unstructured structure, e.g., no headings, blurring screenshots/pictures."
 }
 
